e11762c0aca9c99d1a3de934159bbb4fe721f38a
lokeshVishwakarmajs <101210016+lokeshVishwakarmajs@users.noreply.github.com>
2022-03-09 13:53:47 +0530

    Update index.js

--- test_integration/api/index.js
+++ test_integration/api/index.js
@@ -3,13 +3,11 @@ const router = require("express").Router();
 const fs = require('fs');
 const path = require("path");
 
-router.get("/test", async(req, res) => {
+router.get("/ourendpoint", async(req, res) => {
     try {
-        const url = 'http://netconnect.bluedart.com/Ver1.9/Demo/ShippingAPI/Finder/ServiceFinderQuery.svc';
+        const url = 'https1://netconnect.bluedart.com/Ver1.9/Demo/ShippingAPI/Finder/ServiceFinderQuery.svc';
         const sampleHeaders = {
-        // 'user-agent': 'sampleTest',
-        'Content-Type': 'text/xml;charset=UTF-8',
-        // 'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+            'Content-Type': 'text/xml;charset=UTF-8',
         };
         const xml = fs.readFileSync(path.join(__dirname, 'test/ServiceFinderQuery.xml'), 'utf-8');
         const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml });
@@ -17,6 +15,23 @@ router.get("/test", async(req, res) => {
         res.status(200).json({ data: { headers, body, statusCode }})
     } catch (error) {
         console.log(error.message || error);
+        res.status(200).json({ data: { error: error.message || error }})
+    }
+});
+
+router.get("/w3schoolendpoint", async(req, res) => {
+    try {
+        const url = 'https://www.w3schools.com/xml/tempconvert.asmx';
+        const sampleHeaders = {
+            'Content-Type': 'text/xml',
+        };
+        const xml = fs.readFileSync(path.join(__dirname, 'test/tempconvert.xml'), 'utf-8');
+        const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml });
+        const { headers, body, statusCode } = response;
+        res.status(200).json({ data: { headers, body, statusCode }})
+    } catch (error) {
+        console.log(error.message || error);
+        res.status(200).json({ data: { error: error.message || error }})
     }
 });
 
